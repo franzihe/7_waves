e0b74d76f7ece6098796a80aaceb0c403dca1665
franzihe <franzihe@geo.uio.no>
2021-05-19 21:58:52 +0200

    removed function and put into loop

--- run_ginie.js
+++ run_ginie.js
@@ -11,64 +11,64 @@ var length_long;
 var Sleeve_H;
 var flangeradius;
 
-function export_rectangular(length_short, length_long, Sleeve_H, flangeradius, out_file) {
-    // code to be executed
+// function export_rectangular(length_short, length_long, Sleeve_H, flangeradius, out_file) {
+//     // code to be executed
 
-    CornerA = Point(-length_short / 2, -length_long / 2, 10 m);
-    CornerB = Point(-length_short / 2, length_long / 2, 10 m);
-    CornerC = Point(length_short / 2, length_long / 2, 10 m);
-    CornerD = Point(length_short / 2, -length_long / 2, 10 m);
-    MidAB = Point(-length_short / 2, 0, 10 m);
-    MidBC = Point(0, length_long / 2, 10 m);
-    MidCD = Point(length_short / 2, 0, 10 m);
-    MidAD = Point(0, -length_long / 2, 10 m);
-
-
-
-    line_A_AD = CreateLineTwoPoints(CornerA, MidAD);
-    line_A_AB = CreateLineTwoPoints(CornerA, MidAB);
-    line_B_AB = CreateLineTwoPoints(CornerB, MidAB);
-    line_B_BC = CreateLineTwoPoints(CornerB, MidBC);
-    line_C_BC = CreateLineTwoPoints(CornerC, MidBC);
-    line_C_CD = CreateLineTwoPoints(CornerC, MidCD);
-    line_D_CD = CreateLineTwoPoints(CornerD, MidCD);
-    line_D_AD = CreateLineTwoPoints(CornerD, MidAD);
-
-
-    Radius_A = CreateCircularFilletBetweenLinearSegments(line_A_AB, MidAB, line_A_AD, MidAD, flangeradius);
-    Radius_B = CreateCircularFilletBetweenLinearSegments(line_B_AB, MidAB, line_B_BC, MidBC, flangeradius);
-    Radius_C = CreateCircularFilletBetweenLinearSegments(line_C_BC, MidBC, line_C_CD, MidCD, flangeradius);
-    Radius_D = CreateCircularFilletBetweenLinearSegments(line_D_AD, MidAD, line_D_CD, MidCD, flangeradius);
+//     CornerA = Point(-length_short / 2, -length_long / 2, 10 m);
+//     CornerB = Point(-length_short / 2, length_long / 2, 10 m);
+//     CornerC = Point(length_short / 2, length_long / 2, 10 m);
+//     CornerD = Point(length_short / 2, -length_long / 2, 10 m);
+//     MidAB = Point(-length_short / 2, 0, 10 m);
+//     MidBC = Point(0, length_long / 2, 10 m);
+//     MidCD = Point(length_short / 2, 0, 10 m);
+//     MidAD = Point(0, -length_long / 2, 10 m);
 
 
 
-    Pl_A_up = SweepCurve(Radius_A, Vector3d(0, 0, Sleeve_H / 2));
-    Pl_A_dn = SweepCurve(Radius_A, Vector3d(0, 0, -Sleeve_H / 2));
-    Pl_B_up = SweepCurve(Radius_B, Vector3d(0, 0, Sleeve_H / 2));
-    Pl_B_dn = SweepCurve(Radius_B, Vector3d(0, 0, -Sleeve_H / 2));
-    Pl_C_up = SweepCurve(Radius_C, Vector3d(0, 0, Sleeve_H / 2));
-    Pl_C_dn = SweepCurve(Radius_C, Vector3d(0, 0, -Sleeve_H / 2));
-    Pl_D_up = SweepCurve(Radius_D, Vector3d(0, 0, Sleeve_H / 2));
-    Pl_D_dn = SweepCurve(Radius_D, Vector3d(0, 0, -Sleeve_H / 2));
+//     line_A_AD = CreateLineTwoPoints(CornerA, MidAD);
+//     line_A_AB = CreateLineTwoPoints(CornerA, MidAB);
+//     line_B_AB = CreateLineTwoPoints(CornerB, MidAB);
+//     line_B_BC = CreateLineTwoPoints(CornerB, MidBC);
+//     line_C_BC = CreateLineTwoPoints(CornerC, MidBC);
+//     line_C_CD = CreateLineTwoPoints(CornerC, MidCD);
+//     line_D_CD = CreateLineTwoPoints(CornerD, MidCD);
+//     line_D_AD = CreateLineTwoPoints(CornerD, MidAD);
 
 
-    CentrePlate = Plate(Point(-400 mm, -400 mm, 10 m), Point(-400 mm, 400mm, 10 m), Point(400 mm, 400 mm, 10 m), Point(400 mm, -400mm, 10 m));
+//     Radius_A = CreateCircularFilletBetweenLinearSegments(line_A_AB, MidAB, line_A_AD, MidAD, flangeradius);
+//     Radius_B = CreateCircularFilletBetweenLinearSegments(line_B_AB, MidAB, line_B_BC, MidBC, flangeradius);
+//     Radius_C = CreateCircularFilletBetweenLinearSegments(line_C_BC, MidBC, line_C_CD, MidCD, flangeradius);
+//     Radius_D = CreateCircularFilletBetweenLinearSegments(line_D_AD, MidAD, line_D_CD, MidCD, flangeradius);
 
-    tmpArrayOfCurves = JoinMultipleCurves(Array(Radius_B, Radius_C, Radius_D, Radius_A), true);
-    Rename(tmpArrayOfCurves[0], "JoinedFlange");
-    ScrapPlate = CentrePlate.divide(JoinedFlange);
-    Delete(ScrapPlate);
 
-    GenieRules.Meshing.superElementType = file_number;
-    SimplifyTopology();
-    Analysis1.step(1).execute();
-    Analysis1.step(3).execute();
 
-    FemExporter = ExportMeshFem();
-    FemExporter.DoExport(out_file);  // save the variable to the file to use in Gini
+//     Pl_A_up = SweepCurve(Radius_A, Vector3d(0, 0, Sleeve_H / 2));
+//     Pl_A_dn = SweepCurve(Radius_A, Vector3d(0, 0, -Sleeve_H / 2));
+//     Pl_B_up = SweepCurve(Radius_B, Vector3d(0, 0, Sleeve_H / 2));
+//     Pl_B_dn = SweepCurve(Radius_B, Vector3d(0, 0, -Sleeve_H / 2));
+//     Pl_C_up = SweepCurve(Radius_C, Vector3d(0, 0, Sleeve_H / 2));
+//     Pl_C_dn = SweepCurve(Radius_C, Vector3d(0, 0, -Sleeve_H / 2));
+//     Pl_D_up = SweepCurve(Radius_D, Vector3d(0, 0, Sleeve_H / 2));
+//     Pl_D_dn = SweepCurve(Radius_D, Vector3d(0, 0, -Sleeve_H / 2));
 
-    return console.log("File saved: " + out_file)
-}
+
+//     CentrePlate = Plate(Point(-400 mm, -400 mm, 10 m), Point(-400 mm, 400mm, 10 m), Point(400 mm, 400 mm, 10 m), Point(400 mm, -400mm, 10 m));
+
+//     tmpArrayOfCurves = JoinMultipleCurves(Array(Radius_B, Radius_C, Radius_D, Radius_A), true);
+//     Rename(tmpArrayOfCurves[0], "JoinedFlange");
+//     ScrapPlate = CentrePlate.divide(JoinedFlange);
+//     Delete(ScrapPlate);
+
+//     GenieRules.Meshing.superElementType = file_number;
+//     SimplifyTopology();
+//     Analysis1.step(1).execute();
+//     Analysis1.step(3).execute();
+
+//     FemExporter = ExportMeshFem();
+//     FemExporter.DoExport(out_file);  // save the variable to the file to use in Gini
+
+//     return console.log("File saved: " + out_file)
+// }
 
 
 for (i = 0; i < ls.length; i++) {
@@ -84,7 +84,59 @@ for (i = 0; i < ls.length; i++) {
 
             // console.log(length_short, length_long, fr[j], counter)
             out_file        = "C:/DNVGL/Workspaces/GeniE/RectangularSCF/mesh10/T"+counter+".FEM"
-            export_rectangular(length_short, length_long, Sleeve_H, flangeradius, out_file)
+            // export_rectangular(length_short, length_long, Sleeve_H, flangeradius, out_file)
+            CornerA = Point(-length_short / 2, -length_long / 2, 10 m);
+            CornerB = Point(-length_short / 2, length_long / 2, 10 m);
+            CornerC = Point(length_short / 2, length_long / 2, 10 m);
+            CornerD = Point(length_short / 2, -length_long / 2, 10 m);
+            MidAB = Point(-length_short / 2, 0, 10 m);
+            MidBC = Point(0, length_long / 2, 10 m);
+            MidCD = Point(length_short / 2, 0, 10 m);
+            MidAD = Point(0, -length_long / 2, 10 m);
+
+
+
+            line_A_AD = CreateLineTwoPoints(CornerA, MidAD);
+            line_A_AB = CreateLineTwoPoints(CornerA, MidAB);
+            line_B_AB = CreateLineTwoPoints(CornerB, MidAB);
+            line_B_BC = CreateLineTwoPoints(CornerB, MidBC);
+            line_C_BC = CreateLineTwoPoints(CornerC, MidBC);
+            line_C_CD = CreateLineTwoPoints(CornerC, MidCD);
+            line_D_CD = CreateLineTwoPoints(CornerD, MidCD);
+            line_D_AD = CreateLineTwoPoints(CornerD, MidAD);
+
+
+            Radius_A = CreateCircularFilletBetweenLinearSegments(line_A_AB, MidAB, line_A_AD, MidAD, flangeradius);
+            Radius_B = CreateCircularFilletBetweenLinearSegments(line_B_AB, MidAB, line_B_BC, MidBC, flangeradius);
+            Radius_C = CreateCircularFilletBetweenLinearSegments(line_C_BC, MidBC, line_C_CD, MidCD, flangeradius);
+            Radius_D = CreateCircularFilletBetweenLinearSegments(line_D_AD, MidAD, line_D_CD, MidCD, flangeradius);
+
+
+
+            Pl_A_up = SweepCurve(Radius_A, Vector3d(0, 0, Sleeve_H / 2));
+            Pl_A_dn = SweepCurve(Radius_A, Vector3d(0, 0, -Sleeve_H / 2));
+            Pl_B_up = SweepCurve(Radius_B, Vector3d(0, 0, Sleeve_H / 2));
+            Pl_B_dn = SweepCurve(Radius_B, Vector3d(0, 0, -Sleeve_H / 2));
+            Pl_C_up = SweepCurve(Radius_C, Vector3d(0, 0, Sleeve_H / 2));
+            Pl_C_dn = SweepCurve(Radius_C, Vector3d(0, 0, -Sleeve_H / 2));
+            Pl_D_up = SweepCurve(Radius_D, Vector3d(0, 0, Sleeve_H / 2));
+            Pl_D_dn = SweepCurve(Radius_D, Vector3d(0, 0, -Sleeve_H / 2));
+
+
+            CentrePlate = Plate(Point(-400 mm, -400 mm, 10 m), Point(-400 mm, 400mm, 10 m), Point(400 mm, 400 mm, 10 m), Point(400 mm, -400mm, 10 m));
+
+            tmpArrayOfCurves = JoinMultipleCurves(Array(Radius_B, Radius_C, Radius_D, Radius_A), true);
+            Rename(tmpArrayOfCurves[0], "JoinedFlange");
+            ScrapPlate = CentrePlate.divide(JoinedFlange);
+            Delete(ScrapPlate);
+
+            GenieRules.Meshing.superElementType = file_number;
+            SimplifyTopology();
+            Analysis1.step(1).execute();
+            Analysis1.step(3).execute();
+
+            FemExporter = ExportMeshFem();
+            FemExporter.DoExport(out_file);  // save the variable to the file to use in Gini
             
         }
     }
